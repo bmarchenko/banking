f684845695a8810e308109bb562f4077c566a7b2
Bogdan Marchenko <bogdan.marko@gmail.com>
2011-06-21 15:37:30 +0300

    new version

--- finalBanking.py
+++ finalBanking.py
@@ -1,72 +1,78 @@
 
 #! /usr/bin/python
 
-choice = raw_input ("Dear client, choose type of deposit you'd like to make: simple, compound or bonus: ")
+from math import*
+
+#basic banking deposit class
+class deposit: #1% a month simple interest
+        
+    def __init__(self, name, depsum, depperiod):
+        self.name = name
+        self.depsum = depsum
+        self.depperiod = depperiod
+        
+
+    def calc (self):
+        self.calc = depsum*depperiod*0.01+depsum
+
+    def printDetails (self):
+        print "Dear ", self.name, "your final sum after ", self.depperiod, " monthes will be ", self.calc, "UAH"
+        
+
+
+choice = raw_input ("Choose type of deposit you'd like to make: simple, compound or bonus: ")
+
 if choice == "simple":
         #simple interest as basic
         #restriction of deposit period not less than 6 monthes
-        from bankingParent import*
+        
         class simpledeposit (deposit):
-          pass
-        name = raw_input ("Dear client, enter your Name, please: ")
-        depsum = float(raw_input ("Please, enter the deposit sum: "))
-        depperiod = float(raw_input ("Please, enter the deposit period in month: "))
+                pass
+        
+        name = raw_input ("Enter your Name, please: ")
+        depsum = float(raw_input ("Enter the deposit sum: "))
+        depperiod = float(raw_input ("Enter the deposit period in monthes: "))
         while depperiod < 6:
             print "Dear client, deposit period must be at least 6 monthes"
             depperiod = float(raw_input ("Please, enter the deposit period in month: "))
-        interest = depperiod*depsum*0.01
-        depfinal = depsum+interest
-        Deposit = simpledeposit (name, depsum, depperiod, depfinal, interest)
+        Deposit = simpledeposit (name, depsum, depperiod)
+
+        Deposit.calc()
 
         print Deposit.printDetails()
 
+        
 elif choice == "compound":
         #COMPOUNDED INTEREST
         #Quarterly compounding
-        from bankingParent import*
         class compdeposit (deposit):
-            pass
-        name = raw_input ("Dear client, enter your Name, please: ")
-        depsum = float(raw_input ("Please, enter the deposit sum: "))
-        depperiod = float(raw_input ("Please, enter the deposit period in month: "))
-        interest = depsum*(1.025**(depperiod*0.33))-depsum
-        depfinal = depsum+interest
-        Deposit = compdeposit (name, depsum, depperiod, depfinal, interest)
+            def calc (self):
+                self.calc = depsum*(1.025**(depperiod*0.33))
+                
+        name = raw_input ("Enter your Name, please: ")
+        depsum = float(raw_input ("Enter the deposit sum: "))
+        depperiod = float(raw_input ("Enter the deposit period in monthes: "))
+        Deposit = compdeposit (name, depsum, depperiod)
 
+        Deposit.calc()
+        
         print Deposit.printDetails()
 
 elif choice == "bonus":
         #bonus  2% of initial sum after every 6 monthes
-        from bankingParent import*
-        from math import*
-
-        class bonusdeposit:
-            def __init__(self, name, depsum, depperiod, depfinal, interest, bonus):
-                self.name = name
-                self.depsum = depsum
-                self.depperiod = depperiod
-                self.depfinal = depfinal
-                self.interest = interest
-                self.bonus = bonus
-        
-            def printDetails (self):
-                print "Dear ", self.name, "your final sum after ", self.depperiod, " monthes will be ", self.depfinal, "UAH"
-                print "DETAILS: "
-                print "Name: ", self.name
-                print "Deposit period, monthes: ", self.depperiod
-                print "Initial deposit sum: ", self.depsum
-                print "Final deposit sum: ", self.depfinal
-                print "Interest sum: ", self.interest
-                print "Bonus:", self.bonus
+        class bonusdeposit (deposit):
+            def bonus (self):
+                self.bonus = floor (depperiod/6) * depsum * 0.02
+            def calc (self):
+                self.calc = depsum*(1.025**(depperiod*0.33)) + self.bonus 
         name = raw_input ("Dear client, enter your Name, please: ")
         depsum = float(raw_input ("Please, enter the deposit sum: "))
         depperiod = float(raw_input ("Please, enter the deposit period in month: "))
-        bonus = floor (depperiod/6) * depsum * 0.02
-        interest = depperiod*depsum*0.01
-        depfinal = depsum+interest+bonus
-        Deposit = bonusdeposit (name, depsum, depperiod, depfinal, interest, bonus)
+        Deposit = bonusdeposit (name, depsum, depperiod)
 
+        Deposit.bonus()
+        Deposit.calc()
+        
         print Deposit.printDetails()
 
-else:
-    print "Wrong choice, take care!"
+
