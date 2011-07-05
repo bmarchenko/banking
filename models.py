ba4ea948388675e32b79d470f02ec49208898775
Bogdan Marchenko <bogdan.marko@gmail.com>
2011-07-05 05:00:29 -0400

    updated

--- models.py
+++ models.py
@@ -4,10 +4,31 @@ class Deposit(models.Model):
     name = models.CharField(max_length=30)
     depsum = models.CharField(max_length=30)
     depperiod = models.CharField(max_length=30)
-    deptype = models.CharField(max_length=30)
+    
 
+    def restrict (self):
+                 
+        depperiod > 6
+
+    
+    def calc (self):
+        self.calc = depsum*depperiod*0.01+depsum
 
     def __unicode__(self):
         return u'%s %s %s %s' % (self.name, self.depsum, self.depperiod, self.deptype)
 
+class SimpleDeposit (Deposit):
+                pass
+
+class CompDeposit (Deposit):
+      def calc (self):
+        self.calc = depsum*(1.025**(depperiod*0.33))
+
+class BonusDeposit (Deposit):
+
+    def bonus (self):
+        self.bonus = floor (depperiod/6) * depsum * 0.02
+
+    def calc (self):
+        self.calc = depsum*(1.025**(depperiod*0.33)) + self.bonus
 
