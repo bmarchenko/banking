a08236e1838ddefa32063dee51d6adf0b0adc156
Bogdan Marchenko <bogdan.marko@gmail.com>
2011-07-06 11:13:06 -0400

    Another version

--- views.py
+++ views.py
@@ -10,15 +10,15 @@ def deposit_result(request):
     depperiod = request.POST['depperiod']
     depsum = request.POST['depsum']
     deptype = request.POST['deptype']
-    deposit = (name, depsum, depperiod)
-      
 
-        if deptype = 'simple':
-	   depfinal = deposit.simplecalc()
-	elif deptype = 'compound':
-	  depfinal = deposit.compcalc()
-	else:
-	  depfinal = deposit.bonuscalc()
+    deposit = Deposit(name, depperiod, depsum)
+    if deptype == 'simple':
+        depfinal = deposit.simplecalc()
+    elif deptype = 'compound':
+        depfinal = deposit.compcalc()
+    else:
+        depfinal = deposit.bonuscalc()
+
     return render_to_response('deposit_result.html', 
 {'name' : name, 'depperiod' : depperiod, 'deptype' : deptype, 'depfinal' : depfinal }
 		) 
